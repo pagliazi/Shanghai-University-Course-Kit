82423c3ab02b93b1f35c2d3404cc96d87f98e00d
Layman <laizenan@gmail.com>
2016-04-05 00:42:50 +0800

    Some trifle modification

--- Attacking.py
+++ Attacking.py
@@ -1,5 +1,4 @@
 #coding:utf-8
-import random
 
 import random
 import time
@@ -13,11 +12,22 @@ import re
 import webbrowser
 n=0
 url='http://xk.shu.edu.cn:8080/CourseSelectionStudent/CtrlViewQueryCourseCheck'
-SessionId = '0nzwxfohxr1pehkyv2kl5ike'
+StudentNum='123456'
+WithdCourse='01014127'
+WithdCourse_tnum='1014'
+CourseNum1='01014127'
+CourseTNum1='1014'
+CourseNum2=''
+CourseTNum2=''
+def readsession():
+    cookie=open('cookie.txt').read()
+    pattern= re.compile('ASP.NET_SessionId=(........................);*?')
+    return re.search(pattern,cookie).group(1)
+SessionId = readsession()
 def tuike():
     Tuikeurl ='http://xk.shu.edu.cn:8080/CourseReturnStudent/CtrlViewOperationResult'
     headers = {'Origin': 'http://xk.shu.edu.cn:8080', 'Content-Length': '59', 'Accept-Language': 'zh-CN,zh;q=0.8', 'Accept-Encoding': 'gzip, deflate', 'X-Requested-With': 'XMLHttpRequest', 'Host': 'xk.shu.edu.cn', 'Accept': '*/*', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2552.0 Safari/537.36', 'Connection': 'keep-alive', 'Cookie': '_ga=GA1.3.548295607.1450860734; Hm_lvt_444bf10f6d7469654b7f41f9f9f9c301=1449989162,1449989164,1451806133,1452091199; ASP.NET_SessionId=' +SessionId , 'Referer': 'http://xk.shu.edu.cn:8080/CourseSelectionStudent/FuzzyQuery', 'Content-Type': 'application/x-www-form-urlencoded'}
-    data2='ListCourseStr=01014127%7C1014&StuNo=15123005&Absolute=false'
+    data2='ListCourseStr='+WithdCourse+'%7C'+WithdCourse_tnum+'&StuNo='+StudentNum+'&Absolute=false'
     request=urllib2.Request(Tuikeurl,data2,headers)
     response = urllib2.urlopen(request)
     res=response.read()
@@ -27,45 +37,47 @@ def tuike():
     mat2= re.search('失败',res)
 
     if tuires!= None :
-        print'\n退课成功，现在要开始选课'
+        print( 'Success')
     elif mat2 != None :
-        print '\n退课失败！'
+        print ('\n退课失败！')
         winsound.PlaySound('1.wav',winsound.SND_ASYNC)
     else :
-        print res
-        print '似乎除了点问题'
+        print (res)
+        print ('似乎除了点问题')
         winsound.PlaySound('1.wav',winsound.SND_LOOP)
 def bang():
     global url
     global SessionId
     headers = {'Origin': 'http://xk.shu.edu.cn:8080', 'Content-Length': '206', 'Accept-Language': 'zh-CN,zh;q=0.8', 'Accept-Encoding': 'gzip, deflate', 'X-Requested-With': 'XMLHttpRequest', 'Host': 'xk.shu.edu.cn', 'Accept': '*/*', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2552.0 Safari/537.36', 'Connection': 'keep-alive', 'Cookie': '_ga=GA1.3.548295607.1450860734; Hm_lvt_444bf10f6d7469654b7f41f9f9f9c301=1449989162,1449989164,1451806133,1452091199; ASP.NET_SessionId=' +SessionId , 'Referer': 'http://xk.shu.edu.cn:8080/CourseSelectionStudent/FuzzyQuery', 'Content-Type': 'application/x-www-form-urlencoded'}
-    data= {'stuNo': '15123005', 'IgnorCourseGroup': 'false', 'ListCourseStr': '01014127|1014|0', 'IgnorClassMark': 'false', 'IgnorCredit': 'false'}
+    data= {'stuNo': StudentNum, 'IgnorCourseGroup': 'false', 'ListCourseStr': CourseNum1+'|'+CourseTNum1+'|0', 'IgnorClassMark': 'false', 'IgnorCredit': 'false'}
     data2='CourseNo=&CourseName=&TeachNo=&TeachName=%E9%BB%84%E5%B9%B3%E4%BA%AE&CourseTime=&NotFull=false&Credit=&Campus=0&Enrolls=&DataCount=100&MinCapacity=&MaxCapacity=&PageIndex=1&PageSize=8&FunctionString=InitPage'
     request=urllib2.Request(url,data2,headers)
     response = urllib2.urlopen(request)
     res=response.read()
     res=zlib.decompress(res,zlib.MAX_WBITS|32)
-    pat = re.compile('114')
+    pat = re.compile('114')#这里还是根据需要改一下
     susmat = re.search(pat,res)
     errmat = re.search('115',res)
     errmat2 = re.search('异常',res)
     contmat= re.search ('已满',res)
     havemat=re.search('已选',res)
     if susmat != None :
-       print'找到114'
+       print('找到114')
        return True
     elif errmat != None:
         #print '当前'+errmat.group(0) +'人'
         return False
     else :
-        print '啥都没找到'
+        print ('选课系统似乎不在状态')
+        print res
 url2='http://xk.shu.edu.cn:8080/CourseSelectionStudent/CtrlViewOperationResult'
+
 def bang2():
     global url2
     global SessionId
     headers = {'Origin': 'http://xk.shu.edu.cn:8080', 'Content-Length': '110', 'Accept-Language': 'zh-CN,zh;q=0.8', 'Accept-Encoding': 'gzip, deflate', 'X-Requested-With': 'XMLHttpRequest', 'Host': 'xk.shu.edu.cn', 'Accept': '*/*', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2552.0 Safari/537.36', 'Connection': 'keep-alive', 'Cookie': '_ga=GA1.3.548295607.1450860734; Hm_lvt_444bf10f6d7469654b7f41f9f9f9c301=1449989162,1449989164,1451806133,1452091199; ASP.NET_SessionId=' +SessionId , 'Referer': 'http://xk.shu.edu.cn:8080/CourseSelectionStudent/FuzzyQuery', 'Content-Type': 'application/x-www-form-urlencoded'}
-    data= {'stuNo': '15123005', 'IgnorCourseGroup': 'false', 'ListCourseStr': '01014127|1010|0', 'IgnorClassMark': 'false', 'IgnorCredit': 'false'}
-    data2='ListCourseStr=01014127%7C1010%7C0&stuNo=15123005&IgnorClassMark=false&IgnorCourseGroup=false&IgnorCredit=false'
+    data= {'stuNo': StudentNum, 'IgnorCourseGroup': 'false', 'ListCourseStr': CourseNum1+'|'+CourseTNum1+'|0', 'IgnorClassMark': 'false', 'IgnorCredit': 'false'}
+    data2='ListCourseStr='+CourseNum2+'%7C'+CourseTnum2+'%7C0&stuNo='+StudentNum+'&IgnorClassMark=false&IgnorCourseGroup=false&IgnorCredit=false'
     request=urllib2.Request(url2,data2,headers)
     response = urllib2.urlopen(request)
     res=response.read()
@@ -76,22 +88,22 @@ def bang2():
     contmat= re.search ('已满',res)
     havemat=re.search('已选',res)
     if susmat!= None :
-        print'成功选课'
+        print('成功选课')
         return True
     elif errmat !=None :
-        print'异常！'
+        print('异常！')
         return True
     elif errmat2 != None:
-        print '异常出现拉，赶紧来看看！！'
+        print ('异常出现拉，赶紧来看看！！')
         return True
     elif havemat!= None :
-        print '已经选上拉'
+        print ('已经选上拉')
         return True
     elif contmat != None :
-        print '选课进行中...'
+        print ('选课进行中...')
         return False
     else :
-        print '出事拉！啥都没找到！'
+        print ('出事拉！啥都没找到！')
         return True
 while True :
     ret = bang()
